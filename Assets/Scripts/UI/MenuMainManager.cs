adfc4102c982d0ef45b714faf4fb929759e61749
Jacky2611 <jacky2611@gmail.com>
2017-07-29 10:44:07 +0200

    added pleceholder main menu

---
+++ Assets/Scripts/UI/MenuMainManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMainManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void ButtonPlay()
+    {
+        SceneManager.LoadScene("SceneGameMain");
+    }
+
+    public void ButtonAchievements()
+    {
+
+    }
+
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+}
