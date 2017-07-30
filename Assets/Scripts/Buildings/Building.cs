74633d7ebad22d765d712388e9ec0e9b8b63450a
Jacky2611 <jacky2611@gmail.com>
2017-07-30 10:24:58 +0200

    added basic building implementation

---
+++ Assets/Scripts/Buildings/Building.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Collider2D))]
+public abstract class Building : MonoBehaviour {
+
+    public float maxHealth;
+    public float health;
+    public bool activated;
+
+
+	// Use this for initialization
+	public virtual void Start () {
+        this.health = maxHealth;
+        this.activated = false;
+    }
+
+    // Update is called once per frame
+    public virtual void FixedUpdate () {
+		
+	}
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            this.ShowGUI();
+        } else if(Input.GetMouseButtonDown(1)) {
+            this.SwitchState();
+        }
+    }
+
+    public abstract void ShowGUI();
+    public abstract void SwitchState();
+}
---
+++ Assets/Scripts/Buildings/BuildingHQ.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingHQ : Building {
+
+	// Use this for initialization
+	public override void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public override void FixedUpdate () {
+		
+	}
+
+    public override void ShowGUI()
+    {
+        Debug.Log("showing gui");
+    }
+
+    public override void SwitchState()
+    {
+        Debug.Log("activating");
+
+
+        this.activated = !this.activated;
+    }
+}
