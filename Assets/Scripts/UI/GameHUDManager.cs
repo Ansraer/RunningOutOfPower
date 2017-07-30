6192f1b8706e88e92211581fe335a6101ff4d005
d4nin3u <d4nin3u@gmail.com>
2017-07-30 12:59:26 +0200

    Uiiiii Stuff

--- Assets/Scripts/UI/GameHUDManager.cs
+++ Assets/Scripts/UI/GameHUDManager.cs
@@ -1,23 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameHUDManager : MonoBehaviour {
 
-    public GUIText hp;
-    public GUIText energy;
+    public Text health;
+    public Text energy;
+    public Text notification;
 
-    public GUIText notification;
+    public Slider healthSlider;
+    public Slider energySlider;
 
-
+    private EntityPlayer player;
 
 	// Use this for initialization
 	void Start () {
-		
+        energy.text = "Energy: 100";
+        energySlider.value = 1;
+        health.text = "HP: 100";
+        healthSlider.value = 1;
+        sendNotification("Hi");
+
+
+        player = Object.FindObjectOfType<EntityPlayer>();
+    }
+
+    private void Awake()
+    {
+        player = Object.FindObjectOfType<EntityPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float healthN = player.health/player.maxHealth;
+        healthSlider.value = healthN;
+        health.text = "HP: " + player.health.ToString();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void sendNotification(string text)
+    {
+        notification.text = text;
+        notification.CrossFadeAlpha(1, 2,false);
+        notification.CrossFadeAlpha(0, 2, false);
+    }
+    
 }
