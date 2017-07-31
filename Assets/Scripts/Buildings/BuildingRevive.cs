b3ea319b9412fb423a638f85bc777eb15225539b
d4nin3u <d4nin3u@gmail.com>
2017-08-01 00:51:18 +0200

    Moar Prefabs

---
+++ Assets/Scripts/Buildings/BuildingRevive.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingRevive : Building {
+    
+	
+	// Update is called once per frame
+	public override void FixedUpdate () {
+        base.FixedUpdate();	
+	}
+
+    public override void ShowGUI()
+    {
+        GameHUDManager.instance.ShowInfoBox(this, "PowerPlant", "This is an example text.");
+    }
+
+
+}
---
+++ Assets/Scripts/Buildings/BuildingStrongPowerplant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingStrongPowerplant : Building {
+    
+	
+	// Update is called once per frame
+	public override void FixedUpdate () {
+        base.FixedUpdate();	
+	}
+
+    public override void ShowGUI()
+    {
+        GameHUDManager.instance.ShowInfoBox(this, "PowerPlant", "This is an example text.");
+    }
+
+
+}
